24b316e1257ded5a45473f401b3e59074f2eab75
Azeez Adesina <azeezadesina@yahoo.com>
2025-11-11 05:10:32 +0100

    deep modification and requirements update

--- src/WalletServiceProvider.php
+++ src/WalletServiceProvider.php
@@ -9,7 +9,7 @@ class WalletServiceProvider extends ServiceProvider
     public function register()
     {
         // Merge package config
-        $this->mergeConfigFrom(__DIR__ . '/../config/wallet.php', 'wallet');
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'wallet');
 
         // Bind wallet manager as singleton for shared instance
         $this->app->singleton('wallet', function () {
